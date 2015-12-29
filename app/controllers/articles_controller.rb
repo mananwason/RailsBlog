c0f212b82154f4970d98f9ad6c6c43581c57fe4a
mananwason <manan13056@iiitd.ac.in>
2015-12-30 04:32:27 +0530

    controllers basic added, data saved in db

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,27 @@
 class ArticlesController < ApplicationController
 	def new
 	end
+
+	def show
+		@article = Article.find(params[:id])
+	end
+
+	def index
+		@articles = Article.all
+	end
+
+	def create
+		# render plain: params[:article].inspect
+		@article = Article.new(article_args)
+
+		@article.save
+		redirect_to @article
+	end
+
+	private
+		def article_args
+			params.require(:article).permit(:title, :text)
+		end
+
+
 end
